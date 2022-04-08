cc933736bca723e09bd286ee8ab01ba9146cf58d
Amar <Amar@DESKTOP-52F0U9B>
2022-04-08 18:32:11 +0200

    exercise-5-1

---
+++ src/main/java/Excercise6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Excercise6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double tatimi1 = 0.05;
+        double tatimi2 = 0.10;
+        double tatimi3 = 0.15;
+
+        System.out.println("Shkruaj rrogen brutto (-1 perfundon programin): ");
+        double rroga = input.nextDouble();
+
+        while (rroga != -1) {
+
+            if (rroga >= 1 && rroga <= 180) {
+                System.out.println("nuk ka tatim");
+            }
+            else if (rroga > 180 && rroga <= 250) {
+                rroga = rroga - rroga * tatimi1;
+                System.out.println("Rroga netto eshte: " + rroga);
+            }
+            else if (rroga > 250 && rroga <= 800) {
+                rroga = rroga - rroga * tatimi2;
+                System.out.println("Rroga netto eshte: " + rroga);
+            }
+            else if (rroga >= 800) {
+                rroga = rroga - rroga * tatimi3;
+                System.out.println("Rroga netto eshte: " + rroga);
+            }
+            System.out.print("Shkruaj rrogen brutto amar(-1 perfundon programin): ");
+            rroga = input.nextDouble();
+        }
+        System.out.println("THE END00");
+    }
+}
